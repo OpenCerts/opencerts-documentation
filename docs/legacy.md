1e86a0aa09b43d874d89e6e417b82081d5e8fa02
Nebulis <nebounet@gmail.com>
2020-03-02 18:38:13 +0800

    feat: create docusaurus

---
+++ docs/legacy.md
@@ -0,0 +1,16 @@
+---
+id: legacy
+title: Legacy
+sidebar_label: Legacy
+---
+
+- [Documentation for OpenCerts v1](/v1)
+- Schemas:
+  - [1.0](https://schema.opencerts.io/transcripts/1.0)
+  - [1.1](https://schema.opencerts.io/transcripts/1.1)
+  - [1.2](https://schema.opencerts.io/transcripts/1.2)
+  - [1.3](https://schema.opencerts.io/transcripts/1.3)
+  - [1.4](https://schema.opencerts.io/transcripts/1.4)
+  - [1.5](https://schema.opencerts.io/transcripts/1.5)
+
+
---
+++ website/siteConfig.js
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const extlink = require('remarkable-extlink');
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: "User1",
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
+];
+
+const siteConfig = {
+  title: "OpenCerts", // Title for your website.
+  tagline: "An easy way to check and verify your certificates",
+  url: "https://opencerts.io", // Your website URL
+  baseUrl: "/", // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: "opencerts-documentation",
+  organizationName: "Govtech",
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    { label: "v2" },
+    { doc: "index", label: "Docs" },
+    { blog: true, label: "Blog" },
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: "img/favicon.svg",
+  footerIcon: "img/favicon.svg",
+  favicon: "img/favicon.svg",
+
+  /* Colors for website */
+  colors: {
+    primaryColor: "#324353",
+    secondaryColor: "#ff9933",
+
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Govtech`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: "dark"
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ["https://buttons.github.io/buttons.js"],
+
+  // On page navigation for the current documentation page.
+  onPageNav: "separate",
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: "img/favicon.svg",
+  twitterImage: "img/favicon.svg",
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  // docsSideNavCollapsible: true,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  //   repoUrl: 'https://github.com/facebook/test-site',
+  markdownPlugins: [
+    function(md) {
+      extlink(md, {
+        host: 'docs.opencerts.io', // The hrefs that you DON'T want to be external
+      });
+    },
+  ],
+};
+
+module.exports = siteConfig;
